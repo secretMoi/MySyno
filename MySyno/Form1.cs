7fe22e4bdbcb1e79768be5f33def4ee7511d9b03
Quentin <win.moi@hotmail.com>
2020-03-14 14:21:32 +0100

    Ajoutez des fichiers projet.

---
+++ MySyno/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MySyno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SSH ssh = new SSH("192.168.1.20", "Quentin", "ee6f4e2b02", 32);
+            ssh.Connect();
+            button1.Text = SSH.test();
+            ssh.Disconnect();
+        }
+    }
+}
---
+++ MySyno/SSH.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+using Renci.SshNet;
+
+namespace MySyno
+{
+    class SSH
+    {
+        private string user;
+        private string password;
+        private string host;
+        private int port;
+
+        private static SshClient client;
+        private static object output;
+
+        private Thread thread;
+        private static bool threadFini;
+
+        public SSH(string host, string user, string password, int port = 22)
+        {
+            this.user = user;
+            this.password = password;
+            this.host = host;
+            this.port = port;
+        }
+
+        public void Connect()
+        {
+            client = new SshClient(host, port, user, password);
+            Thread threadConnect = new Thread(ThreadConnect);
+            threadConnect.Start();
+            /*client.Connect();
+            output = client.RunCommand("echo test").Result;*/
+        }
+
+        private static void ThreadConnect()
+        {
+            client.Connect();
+            output = client.RunCommand("echo test").Result;
+            threadFini = true;
+        }
+
+        public void Disconnect()
+        {
+            client.Disconnect();
+            client.Dispose();
+        }
+
+        public static string test()
+        {
+            while (threadFini == false) { }
+
+            return output.ToString();
+        }
+
+        public static object Output => output;
+    }
+}
