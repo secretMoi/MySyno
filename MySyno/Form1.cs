27e8ac31c5f9f17456a24dbcf051d82a936c33f6
Quentin <win.moi@hotmail.com>
2020-03-17 22:44:50 +0100

    Début threading

--- MySyno/Form1.cs
+++ MySyno/Form1.cs
@@ -14,8 +14,25 @@ namespace MySyno
         {
             SSH ssh = new SSH("192.168.1.20", "Quentin", "ee6f4e2b02", 32);
             ssh.Connect();
-            button1.Text = SSH.test();
-            ssh.Disconnect();
+
+            ssh.RaiseCustomEvent += SetText;
+            ssh.Test();
+
+            ssh.commande_Event += Resultat;
+            ssh.SendCommand("ls");
+
+
+            //ssh.Disconnect();
+        }
+
+        public void SetText(object sender, CustomEventArgs e)
+        {
+            button1.Text = e.Message;
+        }
+
+        public void Resultat(object sender, CustomEventArgs e)
+        {
+            richTextBox1.Text = e.Message;
         }
     }
 }
--- MySyno/SSH.cs
+++ MySyno/SSH.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Renci.SshNet;
 
@@ -11,49 +13,126 @@ namespace MySyno
         private string host;
         private int port;
 
-        private static SshClient client;
-        private static object output;
+        private SshClient client;
+        private Mutex verrouMutex;
 
-        private Thread thread;
-        private static bool threadFini;
+        // Declare the event using EventHandler<T>
+        public event EventHandler<CustomEventArgs> RaiseCustomEvent;
+        public event EventHandler<CustomEventArgs> commande_Event;
+
 
         public SSH(string host, string user, string password, int port = 22)
         {
+            verrouMutex = new Mutex();
+
             this.user = user;
             this.password = password;
             this.host = host;
             this.port = port;
+
+            client = new SshClient(host, port, user, password);
         }
 
         public void Connect()
         {
-            client = new SshClient(host, port, user, password);
+            //output = client.RunCommand("echo test").Result;
             Thread threadConnect = new Thread(ThreadConnect);
             threadConnect.Start();
-            /*client.Connect();
-            output = client.RunCommand("echo test").Result;*/
         }
 
-        private static void ThreadConnect()
+        private void ThreadConnect()
         {
+            verrouMutex.WaitOne();
             client.Connect();
-            output = client.RunCommand("echo test").Result;
-            threadFini = true;
+            verrouMutex.ReleaseMutex();
         }
 
         public void Disconnect()
         {
-            client.Disconnect();
-            client.Dispose();
+            Thread threadClose = new Thread(Close);
+            threadClose.Start();
         }
 
-        public static string test()
+        public void SendCommand(string commande)
         {
-            while (threadFini == false) { }
-
-            return output.ToString();
+            Thread t = new Thread(() => RunCommand(commande));
+            t.Start();
         }
 
-        public static object Output => output;
+        private void RunCommand(string commande)
+        {
+            verrouMutex.WaitOne();
+            SshCommand resultat = null;
+
+            if (client.IsConnected)
+                resultat = client.RunCommand(commande);
+
+            Commande_Event(new CustomEventArgs(resultat?.CommandText));
+            verrouMutex.ReleaseMutex();
+        }
+
+        private void Close()
+        {
+            verrouMutex.WaitOne();
+
+            if (client.IsConnected)
+                client.Disconnect();
+
+            client.Dispose();
+
+            verrouMutex.ReleaseMutex();
+        }
+
+        public void Test()
+        {
+            // Write some code that does something useful here
+            // then raise the event. You can also raise an event
+            // before you execute a block of code.
+            OnRaiseCustomEvent(new CustomEventArgs("Did something"));
+        }
+
+        protected virtual void Commande_Event(CustomEventArgs e)
+        {
+            // fait un copie si un subscriber se désinscrit entre le if et le handler()
+            EventHandler<CustomEventArgs> handler = commande_Event;
+
+            // handler vaut null si il n'y a aucun subscriber
+            if (handler != null)
+            {
+                // Use the () operator to raise the event.
+                handler(this, e);
+            }
+        }
+
+        // Wrap event invocations inside a protected virtual method
+        // to allow derived classes to override the event invocation behavior
+        protected virtual void OnRaiseCustomEvent(CustomEventArgs e)
+        {
+            // Make a temporary copy of the event to avoid possibility of
+            // a race condition if the last subscriber unsubscribes
+            // immediately after the null check and before the event is raised.
+            EventHandler<CustomEventArgs> handler = RaiseCustomEvent;
+
+            // Event will be null if there are no subscribers
+            if (handler != null)
+            {
+                // Format the string to send inside the CustomEventArgs parameter
+                e.Message += $" at {DateTime.Now}";
+
+                // Use the () operator to raise the event.
+                handler(this, e);
+            }
+        }
+    }
+
+    // Define a class to hold custom event info
+    public class CustomEventArgs : EventArgs
+    {
+        public CustomEventArgs(string s)
+        {
+            Message = s;
+        }
+
+        public string Message { get; set; }
     }
 }
